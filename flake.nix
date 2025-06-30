032b41e1aedf857ab4edf0334cbd8b587753ad18
cjen1 <cjj39@cam.ac.uk>
2025-06-30 22:08:32 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Basic flakes";
+
+  inputs = { flake-utils.url = "github:numtide/flake-utils"; };
+
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages = { };
+        devShell = pkgs.mkShell {
+          inputsFrom = [ ];
+          buildInputs = [ 
+            pkgs.python3
+          ];
+        };
+      });
+}
