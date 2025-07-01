9c8325b640f0c7a56caf520055691070d90cf61f
Alannah King <42938728+ak2022@users.noreply.github.com>
2025-07-01 21:44:00 +0100

    Update process.py

--- process.py
+++ process.py
@@ -1,6 +1,29 @@
 import argparse
 import sys
 
+residue_colour = {
+    "A": "#0174bd",
+    "C": "#eac250",
+    "D": "#0163e3",
+    "E": "#007614",
+    "F": "#9a2ab2",
+    "G": "#01a871",
+    "H": "#d6004e",
+    "I": "#6bdab3",
+    "K": "#a6073a",
+    "L": "#01b8d3",
+    "M": "#ff6f68",
+    "N": "#1d5f23",
+    "P": "#ff92c9",
+    "Q": "#775100",
+    "R": "#aaabff",
+    "S": "#944900",
+    "T": "#6a4184",
+    "V": "#ffa4aa",
+    "W": "#8e3440",
+    "Y": "#b16e65",
+    "-": "#FFFFFF"]
+
 #>name
 #<sequence>
 #>name
@@ -24,7 +47,7 @@ if __name__ == "__main__":
     for i in range(n_seqs):
         max_seq_len = max(max_seq_len, len(data[i]))
     seq_cols = ",".join(
-            f"Pos_{i + 1}" for i in range(max_seq_len)
+            f"Pos_{i + 1},Pos_{i + 1}__colour" for i in range(max_seq_len)
             )
     print(f"sequence_name,{seq_cols}", file=output)
     
@@ -36,6 +59,7 @@ if __name__ == "__main__":
 
         title = meta_line[1:].strip()
         sequence = ",".join(
-                c for c in sequence_line
+                f"{res},{residue_colour[res]}" for res in sequence_line
                 )
+        
         print(f"{title},{sequence}", file=output)
